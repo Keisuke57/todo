f0edb7efa757471073f0d029c8e89b670742dcf5
Keisuke57 <61958757+Keisuke57@users.noreply.github.com>
2020-04-05 14:09:17 +0900

    Update task.js

--- JS/task.js
+++ JS/task.js
@@ -1,8 +1,8 @@
 alert("hello");
 document.addEventListener('DOMContentLoaded',function(){
-  document.getElementById('btn1').addEventListener('click',function(){
+  document.getElementById('btn').addEventListener('click',function(){
     //テキストボックスの値を取得
-    var answer = document.getElementById('answer1');
+    var answer = document.getElementById('answer');
     //<li>要素を生成
     var list = document.createElement('li');
     //テキストノードを生成して、<li>要素の直下に追加
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded',function(){
     var span = document.createElement('span');
     span.classList.add("far fa-trash-alt");
     //<div id="task">を取得
-    var task = document.getElementById('task1');
+    var task = document.getElementById('task');
     //<div>要素の直下に<li>/<i>/<br>要素の順番で追加
     task.appendChild(list);
     task.appendChild(span);
