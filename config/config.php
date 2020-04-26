8e8aeb40f3f820377acafa51547c272915cb61d2
Keisuke57 <61958757+Keisuke57@users.noreply.github.com>
2020-04-26 15:39:51 +0900

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,9 +2,9 @@
 
   //ini_set('display_errors', 1); //開発中のエラー表示
 
-  define('DSN', 'mysql:host=localhost;dbname=users_data');
-  define('DB_USER', 'dbuser');
-  define('DB_PASS', 'arn3dK7');
+  define('DSN', 'mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=heroku_2fae850380f23a0');
+  define('DB_USER', 'ba6ad64945e74b');
+  define('DB_PASS', '8f34625b');
 
-  define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+  define('SITE_URL', 'https://' . $_SERVER['HTTP_HOST']);
 ?>
