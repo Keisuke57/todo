24b1022a7a422ef89ce47a0f14b69d23e3db3baf
Keisuke57 <61958757+Keisuke57@users.noreply.github.com>
2020-05-10 16:18:59 +0900

    Update index.php

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
     <title>タスク管理</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./CSS/stylesOfindex.css">
+    <link rel="stylesheet" href="./CSS/responsive.css">
     <link href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   </head>
@@ -54,14 +55,14 @@
           <span class="fas fa-bars"></span>
         </div>
         <div class="container">
-          <h2>あなたのやる気を楽しく、最大限に。</h2>
+          <h2><span class="indent">あなたのやる気を楽しく、</span><span class="indent">最大限に。</span></h2>
         </div>
       </div>
       <div id="Introduction-wrapper" class="fadein">
         <div class="container">
           <div class="inner">
             <h2>はじめに</h2>
-            <div class="intro">このサイトでは、<span>「ヤバい集中力:鈴木祐（著）」</span>という本のタスク管理法を使ってあなたの集中力を最大にします。集中力を最大限に発揮することで得られる効果はいうまでもなく絶大なものです。集中すれば1時間で終わる作業も「スマホに気を取られて30分無駄いしてしまった、、、」ということが少なくないでしょう。一日の始まりや一日の終わりにタスクを設定して「スマホをいじってしまう」、「作業に集中できない」、「やらなければならない仕事を先延ばしにしてしまう」といった悩みを解消しましょう。</div>
+            <div class="intro">このサイトでは、<span class="emphasis">「ヤバい集中力:鈴木祐（著）」</span>という本のタスク管理法を使ってあなたの集中力を最大にします。集中力を最大限に発揮することで得られる効果は言うまでもなく絶大なものです。集中すれば1時間で終わる作業も「スマホに気を取られて30分無駄にしてしまった、、、」ということが少なくないでしょう。一日の始まりや一日の終わりにタスクを設定して「スマホをいじってしまう」、「作業に集中できない」、「やらなければならない仕事を先延ばしにしてしまう」といった悩みを解消しましょう。</div>
           </div>
         </div>
       </div>
@@ -69,11 +70,11 @@
         <div class="container">
           <div class="inner">
             <h2>報酬感覚プランニング</h2>
-            <p>「報酬感覚プランニング」とは、1970年代からの研究で生まれた大量の集中力に関する対策のから特に効果が高いものを厳選したワークシートです。このワークシートは、大きく分けて3つあります。中長期的な目標を改めて確認してデイリータスクを絞り込む<span>「基本設定シート」</span>、絞り込んだデイリータスクをこなすための毎日使う<span>「実践設定シート」</span>、短期的な作業に取り組むための<span>「即効簡易版シート」</span>の3つです。</p>
+            <p>「報酬感覚プランニング」とは、1970年代からの研究で生まれた大量の集中力に関する対策から特に効果が高いものを厳選したワークシートです。このワークシートは、大きく分けて3つあります。中長期的な目標を改めて確認してデイリータスクを絞り込む<span class="emphasis">「基本設定シート」</span>、絞り込んだデイリータスクをこなすために毎日使う<span class="emphasis">「実践設定シート」</span>、短期的な作業に取り組むための<span class="emphasis">「即効簡易版シート」</span>の3つです。</p>
           </div>
         </div>
       </div>
-      <div id="guide" class="fadein">
+      <div id="guide-wrapper" class="fadein">
         <div class="container">
           <h2>それぞれの目的に合わせてご利用ください。</h2>
           <div class="selectsheets">
